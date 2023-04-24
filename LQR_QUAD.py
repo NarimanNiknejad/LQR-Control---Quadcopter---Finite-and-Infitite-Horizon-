24fa6a1d6b7853c039b34b9cd890f0abe23fd228
NarimanNiknejad <128648323+NarimanNiknejad@users.noreply.github.com>
2023-04-24 11:03:01 -0400

    Update LQR_QUAD.py

--- LQR_QUAD.py
+++ LQR_QUAD.py
@@ -10,6 +10,7 @@ from scipy.linalg import solve_continuous_are, inv, sqrtm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from nonlinearDynamics import g, m, Ix, Iy, Iz
+from numpy.linalg import norm
 
 
 # time instants for simulation
@@ -19,10 +20,10 @@ t = np.arange(0., t_max, time_step)
 
 # Inintial state
 X0 = np.zeros(12)
-tf = 2
+tf = 10
 t_span = [0, tf]
 
-def solve_riccati_continuous(A, B, Q, R, T, P_T):
+def solve_riccati_continuous(A, B, Q, R, P_T,T = tf,t_for_eval = t):
     """
     Solves the Riccati differential equation for a continuous finite time horizon using the terminal value approach.
 
@@ -55,8 +56,9 @@ def solve_riccati_continuous(A, B, Q, R, T, P_T):
     
     t_span = (0, T)
     P_T = P_T.reshape(len(A)**2)
-    sol = solve_ivp(riccati_dynamics, t_span, P_T)
-    P = sol.y[:,-1].reshape((len(A), len(A)))
+    sol = solve_ivp(riccati_dynamics, t_span, P_T,t_eval=t_for_eval)
+    P = sol.y[:,:]
+    # .reshape((len(A), len(A)))
     return P
 
 
@@ -137,7 +139,10 @@ Byaw = np.array(
 
 # solve LQR 
 Ks = []  # feedback gain matrices K for each subsystem
+P_IFT_List = []
+P_FT_List = []
 K_finite = []
+P_FT_List_all = []
 for A, B in ((Ax, Bx), (Ay, By), (Az, Bz), (Ayaw, Byaw)):
     # infite horizon solution 
     n = A.shape[0]
@@ -145,18 +150,30 @@ for A, B in ((Ax, Bx), (Ay, By), (Az, Bz), (Ayaw, Byaw)):
     Q = np.eye(n)
     Q[0, 0] = 10.  # The first state variable is the one we care about.
     R = np.diag([1., ])
-    K, _, _ = lqr(A, B, Q, R)
+    K, P_IFT, _ = lqr(A, B, Q, R)
     Ks.append(K)
+    P_IFT_List.append(P_IFT)
 
+    # fnite time horizon 
     P_T = np.zeros(A.shape)
-    P = solve_riccati_continuous(A, B, Q, R, tf, P_T)
-    K_ = (inv(R) @ B.T @ P).reshape(-1)
-    for i in range(len(K_)):
-        if i%2==1:
-            K_[i] = -K_[i]
-    K = K_.reshape(K.shape)
-    K_finite.append(K)
+    P_FT_List = list()
+    P = solve_riccati_continuous(A, B, Q, R, P_T)
+    P_FT_List.append(P)
+    K_finite_over_time = []
+    for j,_ in enumerate(t):
+    
+        P_ = P[:,len(t)-j-1].reshape((len(A), len(A)))
+        # print(P)
+        K_ = (inv(R) @ B.T @ P_).reshape(-1)
+        # print(K_)
+        for i in range(len(K_)):
+            if i%2==1:
+                K_[i] = -K_[i]
+        K = K_.reshape(B.shape[0])
+        K_finite_over_time.append(K)
 
+    K_finite.append(np.array(K_finite_over_time))
+    P_FT_List_all.append(P_FT_List)
 
 
 #simulate
@@ -201,9 +218,6 @@ def u_finite(x, _t):
     return np.array([UZ, UY, UX, UYaw])
 
 
-
-
-
 # simulate
 
 
@@ -211,10 +225,11 @@ x_nl_finite = odeint(cl_nonlinear, X0, t, args=(u_finite,))
 x_nl = odeint(cl_nonlinear, X0, t, args=(u,))
 
 
-
 # Plot
 
 fig = plt.figure()
+
+
 errors = fig.add_subplot(2, 2, 1)
 x1_plt = fig.add_subplot(2,2,2)
 x2_plt = fig.add_subplot(2,2,3)
@@ -225,8 +240,9 @@ x3_plt = fig.add_subplot(2,2,4)
 errors.plot(t, signalx - x_nl[:, 0], color="r", label='x error (Infinite Time)')
 errors.plot(t, signaly - x_nl[:, 2], color="g", label='y error (Infinite Time)')
 errors.plot(t, signalz - x_nl[:, 4], color="b", label='z error (Infinite Time)')
-
-
+errors.plot(t, signalx - x_nl_finite[:, 0], "r-.", label='x error (Finite Time)')
+errors.plot(t, signaly - x_nl_finite[:, 2], "g-.", label='y error (Finite Time)')
+errors.plot(t, signalz - x_nl_finite[:, 4], "b-.", label='z error (Finite Time)')
 errors.set_title("Position error for reference tracking")
 errors.set_xlabel("time {s}")
 errors.set_ylabel("error")
@@ -262,7 +278,89 @@ x3_plt.set_xlabel("time {s}")
 x3_plt.set_ylabel("z")
 x3_plt.legend(loc='lower right', shadow=True, fontsize='small')
 
+plt.show()  
+
+
+fig1, errors1 = plt.subplots()
+errors1.plot(t, signalx - x_nl[:, 0], color="r", label='x error (Infinite Time)')
+errors1.plot(t, signaly - x_nl[:, 2], color="g", label='y error (Infinite Time)')
+errors1.plot(t, signalz - x_nl[:, 4], color="b", label='z error (Infinite Time)')
+errors1.plot(t, signalx - x_nl_finite[:, 0], "r-.", label='x error (Finite Time)')
+errors1.plot(t, signaly - x_nl_finite[:, 2], "g-.", label='y error (Finite Time)')
+errors1.plot(t, signalz - x_nl_finite[:, 4], "b-.", label='z error (Finite Time)')
+errors1.set_title("Position error for reference tracking")
+errors1.set_xlabel("time {s}")
+errors1.set_ylabel("error")
+errors1.legend(loc='lower right', shadow=True, fontsize='small')
+
+fig1.savefig('errors1.png')
+
+fig2, x1_plt2 = plt.subplots()
+x1_plt2.plot(t,x_nl[:, 0], color="r", label="Infinite Time")
+x1_plt2.plot(t,signalx, color="b", label="command")
+x1_plt2.plot(t,x_nl_finite[:, 0], "g-.", label="Finite Time")
+x1_plt2.set_title("x")
+x1_plt2.set_xlabel("time {s}")
+x1_plt2.set_ylabel("x")
+x1_plt2.legend(loc='lower right', shadow=True, fontsize='small')
+
+fig2.savefig('x1_plt2.png')
+
+fig3, x2_plt2 = plt.subplots()
+x2_plt2.plot(t,x_nl[:, 2], color="r", label="Infinite Time")
+x2_plt2.plot(t,signaly, color="b", label="command")
+x2_plt2.plot(t,x_nl_finite[:, 2], "g-.", label="Finite Time")
+x2_plt2.set_title("x")
+x2_plt2.set_xlabel("time {s}")
+x2_plt2.set_ylabel("x")
+x2_plt2.legend(loc='lower right', shadow=True, fontsize='small')
+
+fig3.savefig('x2_plt2.png')
+
+fig4, x3_plt2 = plt.subplots()
+x3_plt2.plot(t,x_nl[:, 4], color="r", label="Infinite Time")
+x3_plt2.plot(t,signalz, color="b", label="command")
+x3_plt2.plot(t,x_nl_finite[:, 4], "g-.", label="Finite Time")
+x3_plt2.set_title("x")
+x3_plt2.set_xlabel("time {s}")
+x3_plt2.set_ylabel("x")
+x3_plt2.legend(loc='lower right', shadow=True, fontsize='small')
+
+fig4.savefig('x3_plt2.png')
+
+L2_k = list()
+
+for i in range(3):
+    L2_norm_all_k = list()    
+
+    for j in range(len(t)):
+
+
+        L2_norm_k = norm(K_finite[i][j]-Ks[i])
+        L2_norm_all_k.append(L2_norm_k)
+
+    L2_k.append(L2_norm_all_k)
 
 
 
-plt.show()     
+
+
+
+fig5, K_1_plot = plt.subplots()
+K_1_plot.plot(t,L2_k[0], "g-", label="Kx")
+K_1_plot.plot(t,L2_k[1], "r-.", label="Ky")
+K_1_plot.plot(t,L2_k[2], "b-.", label="Kz")
+K_1_plot.set_title("Norm Difference Between Finite and Infinite")
+K_1_plot.set_xlabel("time {s}")
+K_1_plot.set_ylabel("Norm Difference")
+K_1_plot.legend(loc='lower right', shadow=True, fontsize='small')
+fig5.savefig('K_Comparison.png')
+
+
+# fig6, K_z2_plot = plt.subplots()
+
+# K_z2_plot.plot(t,K_finite[2][1], "g-.", label="Finite Time")
+# K_z2_plot.plot(t,Ks[2], "g-.", label="Infinite Time")
+
+# fig6.savefig('K_z1_Comparison.png')
+   
